842dd4d44e7633b13453d83e0beef4354a04e288
zhouys618 <1767703660@qq.com>
2022-07-07 18:12:24 +0800

    1

--- models/users.py
+++ models/users.py
@@ -10,7 +10,10 @@
 # from class_base import Base
 from models.class_base import Base
 from sqlalchemy import Boolean, Column, Integer, String, VARCHAR, BIGINT
+
+
 class User(Base):
-    __tablename__ = "user"
-    # id = Column(Integer,  autoincrement=True)
+    __tablename__ = "users"  # 数据表的名字
+    __table_args__ = {'extend_existing': True}  # 当数据库中已经有该表时，或内存中已声明该表，可以用此语句重新覆盖声明
+
     email = Column(String(128), unique=True, index=True, nullable=False, comment="邮箱")
